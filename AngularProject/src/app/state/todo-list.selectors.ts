0a4e356babe711b6716b8fb1b9037b6b80471d2a
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-09 17:23:10 +0300

    Update todo-list.selectors.ts

--- AngularProject/src/app/state/todo-list.selectors.ts
+++ AngularProject/src/app/state/todo-list.selectors.ts
@@ -1,11 +1,4 @@
-// import { createSelector } from '@ngrx/store';
-// import { Task } from './todo-list.reducer'; 
 
-
-// export const selectTasks = createSelector(
-//   (state: { tasks: Task[] }) => state.tasks, 
-//   (tasks) => tasks 
-// );
 
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { Task } from './todo-list.reducer';
